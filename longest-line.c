7a3d0c465603724e2e2dc2b90304cd9b93b8bd33
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2020-06-02 10:28:09 +0200

    Reading line example

--- longest-line.c
+++ longest-line.c
@@ -1,53 +1,34 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
-char *readLine();
-void copy(char to[], char from[]);
-/* print the longest input line */
-void main()
-{
-    int len; /* current line length */
-    int max;
-    char *input = NULL;  /* maximum length seen so far */
-    char longest[] = ""; /* longest line saved here */
-    max = 0;
-    while ((input = readLine()) > 0)
-    {
-        if (strlen(input) > max)
-        {
-            max = len;
-            copy(longest, input);
-        }
-        input = NULL;
-    }
-    if (max > 0) /* there was a line */
-        printf("%s", longest);
-}
-/* readLine: read a line into s, return length */
-char *readLine()
-{
-    char *line = NULL;
-    char a;
-    int c;
-    while ((c = getchar()) != EOF && c != '\n')
-    {
-        a = c;
-        strcat(line, &a);
-    }
-    if (c == '\n')
-    {
-        a = c;
-        strcat(line, &a);
-    }
-    a = c;
-    strcat(line, &a);
-    return line;
-}
-/* copy: copy 'from' into 'to'; assume to is big enough */
 void copy(char to[], char from[])
 {
     int i;
     i = 0;
-    while (from[i] != '\0')
-        strncat(to, &from[i], 1);
+    while ((to[i] = from[i]) != '\0')
+    {
+        ++i;
+    }
+}
+
+void main()
+{
+    int max = 0;
+    char *line = NULL;
+    char *longestLine = NULL;
+    size_t len = 0;
+    ssize_t read;
+    while ((read = getline(&line, &len, stdin)) != -1)
+    {
+        if (max < read - 1)
+        {
+            max = read - 1;
+            longestLine = realloc(longestLine, read);
+            copy(longestLine, line);
+        }
+    }
+    printf("\nThis is was the longest line: %s", longestLine);
+    printf("\nIt was %d characters long. \n", max);
+
+    free(longestLine);
 }
