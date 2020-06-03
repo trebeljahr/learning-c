ea080495b68da2a8dc52ec75ed36331012634cf8
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2020-06-03 11:24:12 +0200

    Edit reversing example

--- reverse.c
+++ reverse.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int length(char str[])
+int getLength(char str[])
 {
     int l = 0;
     while (str[l] != '\0')
@@ -20,11 +20,28 @@ void reverse(char reversed[], char str[], int len)
     reversed[len] = '\0';
 }
 
-void main()
+void initReverse()
 {
-    char str[] = "Hello there!";
-    int len = length(str);
+    char str[] = "Hello world";
+    int len = getLength(str);
     char reversed[len];
     reverse(reversed, str, len);
+    printf("String: %s\n", str);
     printf("Reversed: %s\n", reversed);
+}
+
+void main()
+{
+    initReverse();
+    char *line = NULL;
+    size_t length = 0;
+    ssize_t read;
+    while (printf("Your string: ") &&
+           (read = getline(&line, &length, stdin)) != -1)
+    {
+        int len = getLength(line);
+        char reversed[len];
+        reverse(reversed, line, len);
+        printf("Reversed: %s\n", reversed);
+    }
 }
